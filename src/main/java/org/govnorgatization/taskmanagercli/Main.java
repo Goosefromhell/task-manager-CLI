45a5675f0e9f3cf2f27247b79c36b3b39cbcacf5
Fiction <goosefromhellyes@gmail.com>
2025-11-28 17:29:47 +0100

    add update/refac/error checkers

--- src/main/java/org/govnorgatization/taskmanagercli/Main.java
+++ src/main/java/org/govnorgatization/taskmanagercli/Main.java
@@ -13,15 +13,16 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    static void write(ObjectMapper mapper, File target,List<Map<String, Object>> list){
+    static void write(ObjectMapper mapper, File target, List<Map<String, Object>> list) {
         mapper.writerWithDefaultPrettyPrinter()
                 .writeValue(target, list);
 
 
     }
+
     static void main(String[] args) {
-        String[] commands = {"add", "update", "list", "delete","--help","mark <tag>", "tags: done, in-progress, todo"};
-        if (args.length < 2 && !Arrays.toString(commands).contains(args[0])) {
+        String[] commands = {"add", "update", "list", "delete", "--help", "mark <tag>", "tags: done, in-progress, todo"};
+        if (!Arrays.toString(commands).contains(args[0])) {
             System.err.println("Usage: java ... Main <name> <description>");
             return;
         }
@@ -29,13 +30,6 @@ public class Main {
         Path target = Path.of(System.getProperty("user.home"),
                 "Buffers", "TaskManager", "test.json");
 
-//            Map<String, Object> map = mapper.convertValue(json, new TypeReference<>() {});
-//            map.put("koka","soka");
-//            taskLoader.header.putAll(map);
-//            System.out.println(map);
-//            for (JsonNode node : json) {
-//                System.out.println(node);
-//            }
         try {
             Files.createDirectories(target.getParent());
 
@@ -51,32 +45,68 @@ public class Main {
                     }
                     break;
                 case "add":
+                    if (args.length >= 2 ) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                       break;
+
+                    }
                     HashMap<String, Object> new_taks = new HashMap<>();
                     new_taks.put("discription", String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
                     new_taks.put("id", String.valueOf(list.size()));
                     list.add(new_taks);
 
-                    write(mapper,target.toFile(),list);
+                    write(mapper, target.toFile(), list);
                     break;
                 case "update":
-                    break;
-                case "list":
-                    for (Map<String, Object> task : list) {
-                        System.out.println(task.get("discription"));
+                    try {
+                        Integer.parseInt(args[1]);
+                    }
+                    catch (NumberFormatException e) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                        break;
+
+                    }
+                    if (args.length < 3 || !Arrays.toString(args).contains(args[2])) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                        break;
+                    }
+                    if (Integer.parseInt(list.getLast().get("id").toString()) < Integer.parseInt(args[1])) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                        break;
                     }
 
-                    break;
+
+                    list.get(Integer.parseInt(args[1])).replace("discription",Arrays.copyOfRange(args, Integer.parseInt(args[2]),args.length-1));
+
+
                 case "mark":
                     if (args.length < 3 || !Arrays.toString(args).contains(args[2])) {
                         System.err.println("Usage: java ... Main <name> <description>");
                         break;
                     }
-                     if(Integer.parseInt(list.getLast().get("id").toString()) < Integer.parseInt(args[1])) {
-                       System.err.println("Usage: java ... Main <name> <description>");
-                       break;
+                    if (Integer.parseInt(list.getLast().get("id").toString()) < Integer.parseInt(args[1])) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                        break;
                     }
                     list.get(Integer.parseInt(args[1])).put("marked", args[2]);
-                    write(mapper,target.toFile(),list);
+                    write(mapper, target.toFile(), list);
+                    break;
+                case "list":
+                    if  (args.length > 1 ) {
+                        System.err.println("Usage: java ... Main <name> <description>");
+                    }
+                    for (Map<String, Object> task : list) {
+                        String result;
+                        try {
+                            result = task.get("discription") + " "+ task.get("marked").toString()  ;
+
+                        }
+                        catch (Exception e) {
+                            result = task.get("discription").toString();
+                        }
+                        System.out.println(result);
+                    }
+
                     break;
 
                 default:
